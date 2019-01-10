45d1f2e3e85993e941f1e6f0ba800e1b98725480
malletan <a.mallet80@gmail.com>
2019-01-11 00:44:53 +0100

    Update Game.java

--- LO02_TOHT/src/theotherhattrick/Game.java
+++ LO02_TOHT/src/theotherhattrick/Game.java
@@ -540,7 +540,7 @@ public class Game extends Observable implements Serializable, Runnable {
 		players[p1].getHand().remove(ind1);
 //		int i = (variant ==  1 ? 0 : players[p1].speak("yeee", 2, players, 'p') ); // à utiliser si on n'a pas décidé quel prop au milieu on veut récupérer.
 		
-		System.out.println("Le joueur " + p1 + " échange prop " + ind1 + " avec le prop " + ind2 + " du joueur " + p2);
+//		System.out.println("Le joueur " + p1 + " échange prop " + ind1 + " avec le prop " + ind2 + " du joueur " + p2);
 		if (p2 == -1) { // Si on veut échanger le prop du joueur dont c'est le tour avec celui du milieu
 			players[p1].setHand(middleProp.get(0), ind1);
 			middleProp.remove(tmp1);
